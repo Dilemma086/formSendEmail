1ab923416143a545e387198a407cc41a8c3224eb
Dilemma086 <Dilemma086@yandex.ru>
2023-10-17 17:22:22 +0300

    remove file

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ http.open('get', '/JSON/data.json', true)
 
 http.send()
 
+const btnArr = Array.from(document.querySelectorAll('.btnMainForm'))
+const bodyVisibleForm = document.querySelector('body')
+
 http.onload = function renderData(){
     let data = JSON.parse(this.responseText)
-    data.map(item => {
-        const objCard = new FormSend(item)
-        objCard.create()
-        // objCard.append();
-    })
+    btnArr.forEach(i => i.addEventListener('click', () => {
+        let nameId = i.parentNode.className
+        data.forEach(item => {
+            if(item.id === nameId){
+                const objCard = new FormSend(item)
+                objCard.create()
+                objCard.append()            
+            }
+        }
+    )}))
 }
 
 class FormSend{
@@ -35,31 +43,36 @@ class FormSend{
         this.value = item.value
     }
     create(){
-        this.elem.classList.add('modalForm')
+        this.elem.classList.add('modalFormFullScreen')
         this.elem.innerHTML = `
+        <div class="modalForm">
+            <span class="closeVisibleForm"><img src="/close.png"></span>
             <h4>${this.title}</h4>
             <form method="post" id="${this.id}">
             <div class="visibleForm">
-                
                 <input name="${this.name}" class="form-control" type="text" placeholder="${this.namePlaceholder}" required="">
                 <input name="${this.old}" class="form-control" type="text" placeholder="${this.oldPlaceholder}" required="">
         		<input name="${this.tel}" class="form-control" type="tel" placeholder="${this.telPlaceholder}" required="" minlength="11" maxlength="11">
 		        <input name="${this.adress}" class="form-control" type="text" placeholder="${this.adressPlaceholder}" required="">
+                <textarea name="${this.text}" class="form-control" placeholder="${this.textPlaceholder}" required=""></textarea> 
             </div>
             <div class="sendForm">
                 <input type="submit" name="submit" class="sendmailForm" value="${this.value}">
             </div>
             </form>
-
+        </div>
         `
+        this.elem.querySelector('.closeVisibleForm').addEventListener('click', this.close.bind(this))
     }
     append(){
-        document.querySelector('body').append(this.elem)
+        bodyVisibleForm.append(this.elem)
     }
+    close(){
+        this.elem.remove()
+        
+    }
+    
 }
 
-class SelectedForm extends FormSend{
-    elem = document.createElement('div');
 
 
-}
